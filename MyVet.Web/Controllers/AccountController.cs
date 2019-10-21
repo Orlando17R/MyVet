a5ddbe8d31d86f14c160c4f06fdc29a20a0267ba
Orlando Rodriguez <orlando.ramiro11@gmail.comm>
2019-10-21 11:51:59 -0400

    vid08

---
+++ MyVet.Web/Controllers/AccountController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MyVet.Web.Models;
+
+namespace MyVet.Web.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(LoginViewModel modell)
+        {
+            //return View();
+            if (!ModelState.IsValid)
+            {
+                
+            }
+
+            return View(modell);
+
+        }
+    }
+}
