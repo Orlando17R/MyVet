218460d0501fcd1c04be1adcd044095ee7363cd1
Orlando Rodriguez <orlando.ramiro11@gmail.comm>
2019-11-04 12:22:20 -0400

    vid 31

--- MyVet.Prism/MyVet.Prism/ViewModels/LoginPageViewModel.cs
+++ MyVet.Prism/MyVet.Prism/ViewModels/LoginPageViewModel.cs
@@ -1,16 +1,69 @@
 ﻿using Prism.Commands;
 using Prism.Mvvm;
+using Prism.Navigation;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 
 namespace MyVet.Prism.ViewModels
 {
-    public class LoginPageViewModel : BindableBase
+    public class LoginPageViewModel : ViewModelBase
     {
-        public LoginPageViewModel()
+        private string _password;
+        private bool _isRunning;
+        private bool _isEnable;
+        private DelegateCommand _loginCommand;
+
+
+        public LoginPageViewModel(
+            INavigationService navigationService): base(navigationService)
         {
+            Title = "MyVet - LOGIN";
+            IsRunning = false;
+            IsEnable = true;
 
         }
-    }
-}
+
+        public DelegateCommand LoginCommand => _loginCommand ?? (_loginCommand = new DelegateCommand(Login));
+
+        public string Email { get; set; }
+
+        public string Password 
+        { 
+            get => _password; 
+            set => SetProperty(ref _password, value); 
+        }
+
+        public bool IsRunning
+        {
+            get => _isRunning;
+            set => SetProperty(ref _isRunning, value);
+        }
+        public bool IsEnable
+        {
+            get => _isEnable;
+            set => SetProperty(ref _isEnable, value);
+        }
+
+
+        private async void Login()
+        {
+            if (string.IsNullOrEmpty(Email))
+            {
+                await App.Current.MainPage.DisplayAlert("Error", "Debe digitar un email.", "Aceptar");
+                return;
+            }
+
+            if (string.IsNullOrEmpty(Password))
+            {
+                await App.Current.MainPage.DisplayAlert("Error", "Debe digitar una contrasena.", "Aceptar");
+                return;
+            }
+
+            await App.Current.MainPage.DisplayAlert("Ok", "Cargando...!", "Aceptar");
+        }
+
+
+
+}//final class
+}//final namespace
