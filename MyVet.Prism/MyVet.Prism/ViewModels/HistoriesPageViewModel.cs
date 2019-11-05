c5b8046e674767a45c94be11b80c47f2a8c0211c
Orlando Rodriguez <orlando.ramiro11@gmail.comm>
2019-11-05 16:40:51 -0400

    vid 42

---
+++ MyVet.Prism/MyVet.Prism/ViewModels/HistoriesPageViewModel.cs
@@ -0,0 +1,49 @@
+﻿using MyVet.Common.Models;
+using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+
+namespace MyVet.Prism.ViewModels
+{
+    public class HistoriesPageViewModel : ViewModelBase
+    {
+        private PetResponse _pet;
+        private ObservableCollection<HistoryResponse> _histories;
+
+        public HistoriesPageViewModel(INavigationService navigationService): base(navigationService)
+        {
+            Title = "Historias";
+        }
+
+        public ObservableCollection<HistoryResponse> Histories
+        {
+            get => _histories;
+            set => SetProperty(ref _histories, value);
+        }
+
+        public PetResponse Pet 
+        {
+            get => _pet;
+            set => SetProperty(ref _pet, value);
+        }
+
+        public override void OnNavigatedTo(INavigationParameters parameters)
+        {
+            base.OnNavigatedTo(parameters);
+
+            if (parameters.ContainsKey("pet"))
+            {
+                Pet = parameters.GetValue<PetResponse>("pet");
+                Title = $"Historias de: {Pet.Name}";
+                //Pet = _pet;
+                Histories = new ObservableCollection<HistoryResponse>(Pet.Histories);
+            }
+        }//final
+
+
+    }
+}
